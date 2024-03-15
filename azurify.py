8ab557435bacf4dfcb9e807f78089ebb09a88f9d
ashbig <ashbigdeli@gmail.com>
2024-03-15 08:52:40 -0400

    adding domains

--- azurify.py
+++ azurify.py
@@ -1,49 +1,19 @@
-I need domain, cosmic, clinvar, civic, gnomad, litvar
-
+#I need domain, cosmic, clinvar, civic, gnomad, litvar
 #the user should input a file that has a sample string if needed, chrom, start, ref, alt, effect, exon#, 
 
 
-
-
-import pandas as pd
-import os
-
-#parse the domain file to meet the below specs
-
+#hardcoded files to remove from git
 domain_file_path = "C:\\dev\\phd\\dt\\resource_data\\uniprot_hg19_domain.txt"
 input_file_path = 'C:\\dev\\phd\\cornell_data\\cornell_withexons_parsed.tsv'
 
-# Read domain data into a DataFrame
-domains_df = pd.read_csv(domain_file_path, sep='\t', header=None, names=['chrom', 'start', 'stop', 'name'], dtype={'chrom': str, 'start': int, 'stop': int, 'name': str})
 
-# Create a DataFrame to store the results
-result_df = pd.DataFrame(columns=['chrom', 'pos', 'DomainName'])
-
-with open(input_file_path, 'r') as input_file:
-    header = next(input_file).rstrip('\n')
-
-    for line in input_file:
-        fields = line.rstrip('\n').split("\t")
-        chrom, pos = fields[0], int(fields[1])
-
-        # Merge domain information based on the chromosome
-        merged_df = pd.merge_asof(result_df, domains_df[domains_df['chrom'] == chrom].sort_values('start'),
-                                  left_on='pos', right_on='start', direction='forward')
-
-        # Check if the position is within the domain range
-        mask = (pos >= merged_df['start']) & (pos <= merged_df['stop'])
-
-        # Update the 'DomainName' column with the domain name or 'NA'
-        merged_df.loc[mask, 'DomainName'] = merged_df.loc[mask, 'name']
-
-        # Append the current line's data to the result DataFrame
-        result_df = pd.concat([result_df, pd.DataFrame({'chrom': [chrom], 'pos': [pos], 'DomainName': ['NA']})])
-
-# Print or further process the result DataFrame
-print(result_df)
-
+#imports
+import pandas as pd
+import argparse
+import catboost
+import os
                                                                                                                                                                                                                                                            
-def add_domain(df)
+def add_domain(df):
     domains_df = pd.read_csv(domain_file_path, sep='\t', header=None, names=['chrom', 'start', 'stop', 'name'], dtype={'chrom': str, 'start': int, 'stop': int, 'name': str})
     result_df = pd.DataFrame(columns=['chrom', 'pos', 'DomainName'])
 
@@ -54,6 +24,52 @@ def merge_keys(df):
         k.set_index("KEY", inplace = True)
         df = ex.join(k)
     return(df)
+
+def print_ascii():
+    print(r"""
+  ___                     _   __        
+ / _ \                   (_) / _|       
+/ /_\ \ ____ _   _  _ __  _ | |_  _   _ 
+|  _  ||_  /| | | || '__|| ||  _|| | | |
+| | | | / / | |_| || |   | || |  | |_| |
+\_| |_//___| \__,_||_|   |_||_|   \__, |
+                                   __/ |
+                                  |___/ 
+          """)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Azurify classifies the pathogencity of small variants based on clinical training labels.")
+    parser.add_argument("-s", "--sample_column_name", required=False, metavar="COLUMN_NAME", type=str, help="Specify the sample column name.")
+    parser.add_argument("-l", "--litvar", action="store_true", required=False, help="Should we ping LitVar to pull publications. Takes 1 second per ping")
+    parser.add_argument("-i", "--input_file", metavar="FILE_PATH", required=False, type=str, help="Specify the input file.")
+    parser.add_argument("-o", "--output_directory", metavar="DIR_PATH", required=False,type=str, help="Specify the output directory.")
+    parser.add_argument("-f", "--output_filename", metavar="OUTPUT_FILENAME", required=False,type=str, help="Specify the output filename.")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Check for required arguments
+    if not args.input_file or not args.output_directory or not args.output_filename:
+        parser.error("Input file, output directory, and output filename are required.")
+
+    return args
+
+def main():
+    # Example usage
+    print_ascii()
+    args = parse_arguments()
+    print("hello")
+    # Access the values of the arguments
+    #print("Sample Column Name:", args.sample_column_name)
+    #print("Include LitVar:", args.litvar)
+    #print("Input File:", args.input_file)
+    #print("Output Directory:", args.output_directory)
+    #print("Output Filename:", args.output_filename)
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 import time
@@ -80,33 +96,36 @@ print("thats a win")
 
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Your program description here.")
+#parse the domain file to meet the below specs
 
-    # Define options
-    parser.add_argument("-h", "--help", action="help", help="Show this help message and exit.")
-    parser.add_argument("-s", "--sample_column_name", metavar="COLUMN_NAME", type=str, help="Specify the sample column name.")
-    parser.add_argument("-l", "--litvar", action="store_true", help="Should we ping LitVar to pull publications. Takes 1 second per ping")
-    parser.add_argument("-i", "--input_file", metavar="FILE_PATH", type=str, help="Specify the input file.")
-    parser.add_argument("-o", "--output_directory", metavar="DIR_PATH", type=str, help="Specify the output directory.")
-    parser.add_argument("-of", "--output_filename", metavar="OUTPUT_FILENAME", type=str, help="Specify the output filename.")
+domain_file_path = "C:\\dev\\phd\\dt\\resource_data\\uniprot_hg19_domain.txt"
+input_file_path = 'C:\\dev\\phd\\cornell_data\\cornell_withexons_parsed.tsv'
 
-    # Parse the command-line arguments
-    args = parser.parse_args()
+# Read domain data into a DataFrame
+#domains_df = pd.read_csv(domain_file_path, sep='\t', header=None, names=['chrom', 'start', 'stop', 'name'], dtype={'chrom': str, 'start': int, 'stop': int, 'name': str})
 
-    # Check for required arguments
-    if not args.input_file or not args.output_directory or not args.output_filename:
-        parser.error("Input file, output directory, and output filename are required.")
+# Create a DataFrame to store the results
+#result_df = pd.DataFrame(columns=['chrom', 'pos', 'DomainName'])
 
-    return args
+with open(input_file_path, 'r') as input_file:
+    header = next(input_file).rstrip('\n')
 
-if __name__ == "__main__":
-    # Example usage
-    args = parse_arguments()
+    for line in input_file:
+        fields = line.rstrip('\n').split("\t")
+        chrom, pos = fields[0], int(fields[1])
 
-    # Access the values of the arguments
-    print("Sample Column Name:", args.sample_column_name)
-    print("Include LitVar:", args.litvar)
-    print("Input File:", args.input_file)
-    print("Output Directory:", args.output_directory)
-    print("Output Filename:", args.output_filename)
+        # Merge domain information based on the chromosome
+        merged_df = pd.merge_asof(result_df, domains_df[domains_df['chrom'] == chrom].sort_values('start'),
+                                  left_on='pos', right_on='start', direction='forward')
+
+        # Check if the position is within the domain range
+        mask = (pos >= merged_df['start']) & (pos <= merged_df['stop'])
+
+        # Update the 'DomainName' column with the domain name or 'NA'
+        merged_df.loc[mask, 'DomainName'] = merged_df.loc[mask, 'name']
+
+        # Append the current line's data to the result DataFrame
+        result_df = pd.concat([result_df, pd.DataFrame({'chrom': [chrom], 'pos': [pos], 'DomainName': ['NA']})])
+
+# Print or further process the result DataFrame                                                                                                                                                                                                                                                                                                                                                                                                                
+print(result_df)
